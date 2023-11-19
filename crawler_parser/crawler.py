7e0e3505ae2f10c7506b7d2a227f2e113c3967c8
hnguyen07 <54465701+hnguyen07@users.noreply.github.com>
2023-11-19 15:42:37 -0800

    Uploaded the first complete version - further edits might be needed

---
+++ crawler_parser/crawler.py
@@ -0,0 +1,131 @@
+# -------------------------------------------------------------------------
+# AUTHOR: Harry Nguyen
+# FILENAME: crawler.py
+# SPECIFICATION: Crawl the CPP CS Website until the Permanent Faculty page is found.
+#                The target URL is https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml.
+#                The seed URL for the frontier is https://www.cpp.edu/sci/computer-science/.
+#                From the seed page, search through all linked pages until the target is found. Links might appear with
+#                full or relative addresses, and the crawler needs to consider this.
+#                Stop criteria: when the crawler finds the "Permanent Faculty" heading on the page body.
+#                For each traversed page, persist the page's URL along with HTML content in MongoDB collection.
+# FOR: CS 4250- Assignment #3
+# TIME SPENT: 4 hours
+# -----------------------------------------------------------*/
+
+# importing some Python libraries
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import regex as re
+from pymongo import MongoClient
+
+
+# Create a Frontier class to simulate a queue for the crawler to work on
+class Frontier:
+    """ Simulate a Queue class """
+    def __init__(self):
+        self.urls = []
+
+    def is_empty(self):
+        return len(self.urls) == 0
+
+    def add_url(self, url):
+        self.urls.append(url)
+
+    def next_url(self):
+        if not self.is_empty():
+            return self.urls.pop(0)
+        else:
+            raise IndexError("Queue is empty")
+
+    def size(self):
+        return len(self.urls)
+
+    def done(self):
+        return not bool(self.urls)
+
+
+def retrieve_url(url):
+    """ Get the HTML content of the URL """
+    try:
+        html = urlopen(url)
+        # CPP websites are originally encoded in binary form
+        return html.read().decode("utf-8")
+    except HTTPError as e:
+        print(f"Error retrieving URL {url}: {e}")
+        return None
+    except URLError as e:
+        print('The server could not be found!')
+        return None
+
+
+def store_page(url, html, counter):
+    """ Stores the page as a document in MongoDB database"""
+    if html:
+        # Create an instance of MongoClient and informing the connection string
+        client = MongoClient(host=['localhost:27017'])
+        # Create a database
+        db = client.cs_website
+        # Create a collection to save the pages
+        pages = db.pages
+        # Save the URL and HTML content
+        page = {'_id': counter, 'url': url, 'html': html}
+        pages.insert_one(page)
+        # Announce that the pages is stored successfully
+        print(f"Stored page from {url}")
+
+
+def target_page(html):
+    """ Detect if the crawler reaches the target page"""
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.find('h1', string='Permanent Faculty')
+
+
+def parse(html):
+    """ Traverse through the HTML content to discover all available URLs of other pages"""
+    bs = BeautifulSoup(html, 'html.parser')
+    # Extract all links from the page
+    links = bs.find_all('a', href=True)
+    return [link['href'] for link in links]
+
+
+def crawler_thread(frontier):
+    """ The Crawler to crawl the URLs and HTML contents"""
+    crawled_pages = []  # List of crawled pages to check for duplicate before adding to the frontier
+    counter = 0  # id of the page when adding to the MongoDB collection
+    while not frontier.done():
+        counter += 1
+        url = frontier.next_url()
+        crawled_pages.append(url)
+        html = retrieve_url(url)
+        store_page(url, html, counter)
+
+        if target_page(html):
+            print(f"Target page found: {url}")
+            frontier = Frontier()  # Clear the frontier
+        else:
+            for new_url in parse(html):
+                is_abs_link = re.search('^http', new_url)
+                if is_abs_link:
+                    full_url = new_url
+                else:
+                    # urljoin will help join paths and handle relative path
+                    full_url = urljoin(url, new_url)
+
+                # Check for duplicates before adding to the frontier
+                if full_url not in frontier.urls and full_url not in crawled_pages:
+                    frontier.add_url(full_url)
+
+
+def main():
+    """ Main Program to run the crawler """
+    base_url = "https://www.cpp.edu/sci/computer-science/"
+    frontier = Frontier()
+    frontier.add_url(base_url)
+    crawler_thread(frontier)
+
+
+if __name__ == '__main__':
+    main()
---
+++ crawler_parser/parser.py
@@ -0,0 +1,124 @@
+# -------------------------------------------------------------------------
+# AUTHOR: Harry Nguyen
+# FILENAME: db_connection_mongo_solution.py
+# SPECIFICATION: Read the CS Faculty information, parse faculty members name, title, office, email, and website, and
+#                persist this data in MongoDB collection.
+# FOR: CS 4250- Assignment #3
+# TIME SPENT: 2 hours
+# -----------------------------------------------------------*/
+
+# importing some Python libraries
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import regex as re
+
+
+def connectDataBase():
+    # Create a database connection object using pymongo
+    DB_NAME = 'cs_website'
+    DB_HOST = 'localhost'
+    DB_PORT = 27017
+
+    try:
+        # Create an instance of MongoClient
+        client = MongoClient(host=DB_HOST, port=DB_PORT)
+        # Create a database
+        db = client[DB_NAME]
+        return db
+    except:
+        print('Database not connected successfully')
+
+
+def main():
+    # Connect to the database
+    db = connectDataBase()
+    pages = db.pages
+    # Create a collection to store the professors' info
+    professors = db.professors
+
+    query = {'url': 'https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml'}
+    # Retrieve the record of the Permanent Faculty page from the database based on the provided URL
+    result = pages.find_one(query)
+
+    # Check if the HTML content was found
+    if not result:
+        print('No matching records found')
+    else:
+        # Get the HTML content of the URL
+        html = result['html']
+        print('HTML content of CS Permanent Faculty extracted successfully!')
+        bs = BeautifulSoup(html, 'html.parser')
+
+        # Create lists for each data field
+        prof_names = []
+        prof_titles = []
+        prof_offices = []
+        prof_emails = []
+        prof_webs = []
+
+        # Get the professors' names
+        name_list = bs.find_all('h2')
+        for name in name_list:
+            prof_names.append(name.get_text().strip())
+        prof_names = prof_names[1:]  # Exclude the Permanent Faculty text
+
+        # Get the professors' titles
+        title_element = bs.find_all('strong', string=re.compile('Title:?'))
+        for title in title_element:
+            # Get the next sibling, which is the text containing the title
+            prof_title = title.find_next_sibling(string=True).strip()
+            # Remove noise from the data
+            if prof_title[0] == ':':
+                prof_titles.append(prof_title[2:])
+            else:
+                prof_titles.append(prof_title)
+
+            # Get the professors' office numbers (next to title is office number)
+            # Get the next sibling, which is the text containing the office number
+            office_element = title.find_next('strong')
+            office = office_element.find_next_sibling(string=True).strip()
+            # Remove noise from the data
+            if office[0] == ':':
+                prof_offices.append(office[2:])
+            else:
+                prof_offices.append(office)
+
+            # Get the professors' emails (next to office number is phone, next to phone is email)
+            # Have to traverse two times from office section to get to email section
+            email_element = office_element.find_next('strong').find_next('strong')
+            prof_emails.append(email_element.find_next('a')['href'].replace('mailto:', ''))
+
+            # Get the professors' websites (next to email is website)
+            web_element = email_element.find_next('strong')
+            web = web_element.find_next('a')['href']
+            # Remove noise from the data and make the websites' format uniform
+            if web.find('index.shtml') != -1:
+                prof_webs.append(web.replace('index.shtml', ''))
+            else:
+                if re.search('/$', web):
+                    prof_webs.append(web)
+                else:
+                    prof_webs.append(web + '/')
+
+        counter = 1
+        for name, title, office, email, website in zip(prof_names, prof_titles, prof_offices, prof_emails, prof_webs):
+            # Create a record of a professor with the required fields including name, title, office, email, and website
+            professor = dict()
+            professor['_id'] = counter
+            professor['name'] = name
+            professor['title'] = title
+            professor['office'] = office
+            professor['email'] = email
+            professor['website'] = website
+
+            # Print out the professor's record to have a general view of the data
+            print(professor)
+
+            # Insert the professor record to the collection
+            professors.insert_one(professor)
+            # Increment the id number to be used in the database
+            counter += 1
+
+
+if __name__ == '__main__':
+    main()
